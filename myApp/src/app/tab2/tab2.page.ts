9d267245e11155794586333f02c3f141be55e2f5
B45i <amalshajan2011@gmail.com>
2020-09-13 15:49:49 +0530

    new app

--- myApp/src/app/tab2/tab2.page.ts
+++ myApp/src/app/tab2/tab2.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { TextService } from '../text.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  isReverse = false;
+  constructor(
+    public textService: TextService,
+    public toastController: ToastController
+  ) {}
 
-  constructor() {}
-
+  onClick(event: any) {
+    event.target.select();
+    document.execCommand('copy');
+    this.toastController
+      .create({
+        message: 'Text copied to clip board',
+        duration: 2000,
+      })
+      .then((toast) => toast.present());
+  }
 }
---
+++ myApp/src/app/text.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TextService {
+  constructor() {}
+
+  parts: Array<string> = [];
+
+  genarateText(title: string, content: string, footer: string) {
+    let partial = '';
+    content
+      .trim()
+      .split(' ')
+      .forEach((w, i) => {
+        const frag = `${title} - PART: ${
+          this.parts.length + 1
+        }\n\n${partial}\n\n${footer}`;
+        if ((frag + w).length > 2150) {
+          this.parts.push(frag);
+          partial = '';
+        } else {
+          partial = `${partial} ${w}`;
+        }
+      });
+    this.parts.push(
+      `${title} - PART: ${this.parts.length + 1}\n\n${partial}\n\n${footer}`
+    );
+  }
+}
